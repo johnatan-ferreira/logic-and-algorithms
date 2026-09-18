80f401cd748e86bc327105a235abae13ba885428
Johnatan Ferreira <johnatan.fs@protonmail.com>
2026-09-18 12:03:10 -0300

    Adição do 2744

---
+++ 9 - SQL/2744.sql
@@ -0,0 +1,16 @@
+CREATE TABLE account(
+	id INT PRIMARY KEY,
+	name VARCHAR(255),
+	login VARCHAR(255),
+	password VARCHAR(255)
+);
+
+INSERT INTO account(id, name, login, password)
+VALUES	(1, 'Joyce P. Parry', 'Promeraw', 'noh1Oozei'),
+		(2, 'Michael T. Gonzalez', 'Phers1942', 'Iath3see9bi'),
+		(3, 'Heather W. Lawless', 'Hankicht', 'diShono4'),
+		(4, 'Otis C. Hitt', 'Conalothe', 'zooFohH7w'),
+		(5, 'Roger N. Brownfield', 'Worseente', 'fah7ohNg');
+
+SELECT id, password, MD5(password)
+FROM account;
